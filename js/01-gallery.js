d6cd544f27300f8adc17b60d8f5afa5ad63f0eb0
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-09-21 07:17:39 +0300

    Update 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsTamplate = ({ preview, original, description }) =>
+ `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img 
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image ${description}"
+    />
+  </a>
+</div>`;
+
+const refs = {
+ galleryEl: document.querySelector('.gallery'),
+};
+
+const render = () => {
+ const list = galleryItems
+  .map((galleryItem) => galleryItemsTamplate(galleryItem))
+  .join('');
+
+ refs.galleryEl.innerHTML = '';
+ refs.galleryEl.insertAdjacentHTML('beforeend', list);
+};
+
+const handleImgClick = (e) => {
+ console.log(e.target.dataset.source);
+ console.log(e.currentTarget);
+};
+
+//console.log(galleryItems);
+render();
+
+refs.galleryEl.addEventListener('click', handleImgClick);
